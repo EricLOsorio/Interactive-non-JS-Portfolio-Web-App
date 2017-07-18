b08576e965bb57cf6376c5632e1cf56804beee2a
EricLOSorio <erioso@verizon.net>
2017-07-18 16:02:56 -0400

    added ES2015 features to JS files

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,158 +1,243 @@
 window.onload=(function(){
-  var numberKeys=document.getElementsByClassName("nums");
-  var total=document.getElementById("total");
-  var functions=document.getElementsByClassName("functions");
-  var logic=document.getElementsByClassName("logic");
-  var fraction=document.getElementsByClassName("fraction");
+
+  `use strict`;
+
+  let total=document.getElementById(`total`);
+
+  let display=document.getElementById(`display`);
+
+  let numberKeys=document.getElementsByClassName(`nums`);
+
+  let numOnly, //whole number portion of the display
+
+      theDisplay, //display area of the calculator
+
+      theCode, //ASCII code of number key pressed
+
+      newDisplay, //updated display
+
+      frontDisplay, //front portion of display
+
+      answer, //vslue after clicking the equal key
+
+      j=0, //Used to count all characters up to the LATEST symbol
+
+      k=0, //Used in determining what to do when DOT is pressed
+
+      clearFlag=true; //Display is initially 0 or when the `C` key hit;
+
+  for(let i=0; i<numberKeys.length;i++){
+
+     numOnly=``;
+
+     theDisplay=``;
 
 
-  var display=document.getElementById("display"),
-  j=0, //Used to count all characters up to the LATEST symbol
-  k=0, //Used in determining what to do when DOT is pressed
-  clearFlag=true; //Display is initially 0 or when the "C" key hit
+  	numberKeys[i].addEventListener(`click`, event => {
+
+      let thisKey=numberKeys[i];
 
 
-  for(var i=0; i<numberKeys.length;i++){
-
-    var numOnly="";
-    var theDisplay="";
+       theCode=(thisKey.innerHTML).charCodeAt();
 
 
-  	numberKeys[i].addEventListener('click',function(event){
+      if(thisKey.innerHTML!=Number(thisKey.innerHTML) && thisKey.innerHTML!=`.` &&
+          theCode!==177 && thisKey.innerHTML!==`DEL` && thisKey.innerHTML!==`C`) {
 
-      var theCode=(this.innerHTML).charCodeAt();
-
-
-      if(this.innerHTML!=Number(this.innerHTML) && this.innerHTML!="." &&
-          theCode!==177 && this.innerHTML!=="DEL" && this.innerHTML!=="C"){
           if((theDisplay[theDisplay.length-1]==Number(theDisplay[theDisplay.length-1])||
-            theDisplay[theDisplay.length-1]==".") && theCode!==247){
-            theDisplay=theDisplay+this.innerHTML;
+            theDisplay[theDisplay.length-1]==`.`) && theCode!==247) {
+
+            theDisplay=theDisplay+thisKey.innerHTML;
 
             j=theDisplay.length-1;
              k=j;  
 
           } else if(theCode===247){
+
              if (theDisplay[theDisplay.length-1]==Number(theDisplay[theDisplay.length-1]) ||
-              theDisplay[theDisplay.length-1]=="."){
-                 theDisplay=theDisplay+"/";
+              theDisplay[theDisplay.length-1]==`.`) {
+
+                 theDisplay=`${theDisplay}/`;
                  j=theDisplay.length-1;
                  k=j; 
+
             } else {
-              var newDisplay="";
-              for(var n=0;n<theDisplay.length-1;n++){
+
+               newDisplay=``;
+
+              for(let n=0;n<theDisplay.length-1;n++){
+
                newDisplay=newDisplay+theDisplay[n];
           };  
-             theDisplay=newDisplay+"/";  
+             theDisplay=`${newDisplay}/`;  
 
-          };              
+          };  
+
+
               
            } else {
-              var newDisplay=""; 
-              for(var n=0;n<theDisplay.length-1;n++){
-               newDisplay=newDisplay+theDisplay[n];
-          };  
-             theDisplay=newDisplay+this.innerHTML;  
+
+               newDisplay=``; 
+
+              for(let n=0;n<theDisplay.length-1;n++) {
+
+                newDisplay=newDisplay+theDisplay[n];
+              };  
+
+              theDisplay=newDisplay+thisKey.innerHTML;  
 
           };
-      } else if (this.innerHTML=="."){
-             if((theDisplay.substr(k)).indexOf(".")<0){
+
+
+      } else if (thisKey.innerHTML==`.`) {
+
+             if((theDisplay.substr(k)).indexOf(`.`)<0) {
+
               if(theDisplay[theDisplay.length-1]!=Number(theDisplay[theDisplay.length-1])){
-               theDisplay=theDisplay+"0.";
+
+               theDisplay=`${theDisplay}0.`;
 
                j=theDisplay.length-1;
-             } else theDisplay=theDisplay+".";
-              
+
+             } else theDisplay=`${theDisplay}.`;
 
                j=theDisplay.length-1;              
 
              };
             
-      } else if(this.innerHTML==Number(this.innerHTML)){
+      } else if(thisKey.innerHTML==Number(thisKey.innerHTML)){
+
             if(clearFlag){
-              theDisplay="";
+
+              theDisplay=``;
+
               clearFlag=false;
             }
-            theDisplay=theDisplay+this.innerHTML;
-               j=theDisplay.length-1;
+
+            theDisplay=theDisplay+thisKey.innerHTML;
+
+            j=theDisplay.length-1;
 
 
       } else if(theCode===177){
 
-            var frontDisplay="";
+             frontDisplay=``;
+
              if(k!==0){
+
                numOnly=theDisplay.substr(k+1);
 
-                for (var m=0;m<k+1;m++){
+                for (let m=0;m<k+1;m++){
+
                   frontDisplay=frontDisplay+theDisplay[m];
+
                  }; 
 
 
-                 if(theDisplay[(k+1)]!=="-"){
-                  numOnly="-"+numOnly;
+                 if(theDisplay[(k+1)]!==`-`){
+
+                  numOnly=`-${numOnly}`;
+
                  } else{
+
                   numOnly=numOnly.substr(1);
+
                  }
 
-                 if(frontDisplay[frontDisplay.length-1]!=="-"){
-                   theDisplay=frontDisplay+numOnly;                   
+                 if(frontDisplay[frontDisplay.length-1]!==`-`){
+
+                   theDisplay=frontDisplay+numOnly; 
+
                  } else {
+
                     numOnly=numOnly.substr(1);
+
                     frontDisplay=frontDisplay.substring(0,(frontDisplay.length-1));
-                    theDisplay=frontDisplay+"+"+numOnly;
+
+                    theDisplay=`${frontDisplay}+${numOnly}`;
                  }
                 
 
                  
 
              } else {
-                if(theDisplay[0]!=="-"){
+
+                if(theDisplay[0]!==`-`){
+
                 numOnly=theDisplay;
-                theDisplay="-"+numOnly;
+
+                theDisplay=`-${numOnly}`;
+
                 } else {
+
                   numOnly=theDisplay.substr(1);
+
                   theDisplay=numOnly;
+
                 }
 
              } 
 
              j=theDisplay.length-1;
 
-        } else if (this.innerHTML==="DEL") {
-          var newDisplay="";
-          for(var n=0;n<theDisplay.length-1;n++){
+        } else if (thisKey.innerHTML===`DEL`) {
+
+           newDisplay=``;
+
+          for(let n=0;n<theDisplay.length-1;n++){
+
             newDisplay=newDisplay+theDisplay[n];
+
           };
           j-=1;
+
           theDisplay=newDisplay;
+
           if(theDisplay[theDisplay.length-1]!=Number(theDisplay[theDisplay.length-1]) &&
-            theDisplay[theDisplay.length-1]!="."){
+            theDisplay[theDisplay.length-1]!=`.`){
+
             if(theDisplay[theDisplay.length-2]!=Number(theDisplay[theDisplay.length-1]) &&
-               theDisplay[theDisplay.length-2]!="."){
+               theDisplay[theDisplay.length-2]!=`.`){
+
                k=j-1;
+
             } else k=j;
+
           };
-        } else if(this.innerHTML==="C") {
-          theDisplay="0";
+
+        } else if(thisKey.innerHTML===`C`) {
+
+          theDisplay=`0`;
+
           clearFlag=true;
+
         };
       
 
          display.value=theDisplay;
   	}); 
 
-      total.addEventListener('click',function(event){
 
-         var answer=display.value;
+
+
+
+      total.addEventListener(`click`, event => {
+
+         answer=display.value;
+
          display.value=eval(answer);
-         theDisplay=eval(answer).toString();
-         j=0;
-         k=0;
-         numOnly="";
-       
-      });
 
-  };
+         theDisplay=eval(answer).toString();
+
+         j=0;
+
+         k=0;
+
+         numOnly=``;
+       
+      }); //eventListener
+
+  }; //for loop
 
 
 }(window.console));
--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,50 +1,68 @@
 window.onload=(function() {
 
-     'use strict';
+     `use strict`;
 
-     var topNav=document.getElementById("topNavBar"); //the top Nav Bar
-     var scrollSideBar=document.getElementById('scrollSideBar'); //The Side Scroll Nav Bar
+     let topNav=document.getElementById(`topNavBar`); //the top Nav Bar
+
+     let scrollSideBar=document.getElementById(`scrollSideBar`); //The Side Scroll Nav Bar
 
 // Define smooth scroll links that point to where you want to go
 
-     var menuLabel=document.querySelectorAll('.topNavBar__menuLabel');
-     var topBarInputs=document.querySelectorAll('input[name=navRadio]');
-     var sideBarInputs=document.querySelectorAll('input[name=sideScroll]');
-     var scrollToTop=document.getElementsByClassName('link2Top')[0];
+     let menuLabel=document.querySelectorAll(`.topNavBar__menuLabel`);
 
-     var large=window.matchMedia("(max-width:1024px)");
-     var defaultScreen=window.matchMedia("(min-width:1025px");
-     var randomNum=1;
-     var factButton=document.getElementById('factButton');
-     var factText=document.getElementById('factText');
+     let topBarInputs=document.querySelectorAll(`input[name=navRadio]`);
+
+     let sideBarInputs=document.querySelectorAll(`input[name=sideScroll]`);
+
+     let scrollToTop=document.getElementsByClassName(`link2Top`)[0];
+
+     let large=window.matchMedia(`(max-width:1024px)`);
+
+     let defaultScreen=window.matchMedia(`(min-width:1025px`);
+
+     let factButton=document.getElementById(`factButton`);
+
 
 /*******************************COOKIE SETTINGS************************************************/
 
-var introMovie=document.getElementById('introMovie');
-var myVideo=document.getElementById('myIntro');
-var enter=document.getElementById('enter');
-var wrapper=document.getElementsByClassName('wrapper')[0];
+    let introMovie=document.getElementById(`introMovie`);
+
+    let myVideo=document.getElementById(`myIntro`);
+
+    let enter=document.getElementById(`enter`);
+
+    let wrapper=document.getElementsByClassName(`wrapper`)[0];
 
 
     if(!document.cookie){
-      introMovie.style.display='block';
+
+      introMovie.style.display=`block`;
+
       myVideo.load();
+
       myVideo.play();
-     //myVideo.setAttribute('style', 'top:20%; transition: top 5s');
-      document.cookie='intro=done';
+
+      document.cookie=`intro=done`;
+
     } else{
-      introMovie.style.display='none';
-      wrapper.style.display="block";
+
+      introMovie.style.display=`none`;
+
+      wrapper.style.display=`block`;
 
     };
 
-enter.addEventListener('click',function(event){
+    enter.addEventListener(`click`, event=>{
 
-  wrapper.style.display="block";
-  introMovie.style.display='none';
-  myVideo.autoplay = false;
-  myVideo.load();
-});
+      wrapper.style.display=`block`;
+
+      introMovie.style.display=`none`;
+
+      myVideo.autoplay = false;
+
+      myVideo.load();
+
+    });
 
 
 
@@ -52,29 +70,44 @@ enter.addEventListener('click',function(event){
 
 
 
-        window.addEventListener('scroll', function(event){
-          //  var posi = window.getComputedStyle(topNav, null).getPropertyValue("position");
-            var loc=window.pageYOffset;
-          // slider variable representing the sliding arrow for the side scroll menu
-            var slider=document.getElementById('slide');
+        window.addEventListener(`scroll`, event=>{
+
+
+            let loc=window.pageYOffset;
+
+          // slider letiable representing the sliding arrow for the side scroll menu
+
+            let slider=document.getElementById(`slide`);
 
           if(!defaultScreen.matches){
 
-           slider.addEventListener('click', function(event){
-              scrollSideBar.style.left="-80px";
-              scrollSideBar.style.transition="left 1s";
-              scrollSideBar.style.zIndex="1000";
-              slider.style.left="-160px";
-              slider.style.transition="0.5s";
+           slider.addEventListener(`click`, event=>{
+
+              scrollSideBar.style.left=`-80px`;
+
+              scrollSideBar.style.transition=`left 1s`;
+
+              scrollSideBar.style.zIndex=`1000`;
+
+              slider.style.left=`-160px`;
+
+              slider.style.transition=`0.5s`;
+
             });
 
 
-            scrollSideBar.addEventListener(('click'), function(event){
-              scrollSideBar.style.left="-160px";
-              slider.style.left="0";
-              scrollSideBar.style.transition="0.5s";
-              slider.style.transition="0.5s";
-              slider.style.zIndex="1000";
+            scrollSideBar.addEventListener((`click`), event=>{
+
+              scrollSideBar.style.left=`-160px`;
+
+              slider.style.left=`0`;
+
+              scrollSideBar.style.transition=`0.5s`;
+
+              slider.style.transition=`0.5s`;
+
+              slider.style.zIndex=`1000`;
+
             },false);
 
           }
@@ -82,91 +115,141 @@ enter.addEventListener('click',function(event){
  
              //Hide the top nav bar
 
-           topNav.style.top="-100px";
-           topNav.style.transition= "top 1s";
+           topNav.style.top=`-100px`;
+
+           topNav.style.transition= `top 1s`;
 
            //Slide in the side bar upon scroll
 
            if(large.matches){
-            //  scrollSideBar.style.left="-80px";
-             // scrollSideBar.style.transition="left 1s";
-              //scrollSideBar.style.zIndex="1000";
-              slider.style.left="0px";
-              slider.style.transition="left 0.5s";
+
+
+              slider.style.left=`0px`;
+
+              slider.style.transition=`left 0.5s`;
+
            } else{
-               scrollSideBar.style.left="0";
-               scrollSideBar.style.transition="left 1s";              
+
+               scrollSideBar.style.left=`0`;
+
+               scrollSideBar.style.transition=`left 1s`; 
+
            }
 
 
            if(loc===0){
-            topNav.style.top="-10px";
-            scrollSideBar.style.left="-160px";
-            scrollToTop.style.display="none";
-            slider.style.left="-150px";
-            slider.style.transition="left 0.5s";
+
+            topNav.style.top=`-10px`;
+
+            scrollSideBar.style.left=`-160px`;
+
+            scrollToTop.style.display=`none`;
+
+            slider.style.left=`-150px`;
+
+            slider.style.transition=`left 0.5s`;
+
            } else{
+
                if(loc!==0 && defaultScreen.matches){
-                 scrollToTop.style.display="block";                
+
+                 scrollToTop.style.display=`block`;  
+
                }
 
            }
 
             if(loc>=0 && loc<=250){
+
                 topBarInputs[0].checked=true;
+
                 sideBarInputs[0].checked=true;
+
             } else if (loc>250 && loc<1300){
+
                 topBarInputs[1].checked=true;
+
                 sideBarInputs[1].checked=true;
+
             } else if(loc>=1300 && loc<2198){
+
                 topBarInputs[2].checked=true;
+
                 sideBarInputs[2].checked=true;;
+
             } else if(loc>2198){
+
                 topBarInputs[3].checked=true;
+
                 sideBarInputs[3].checked=true;
+
             }
 
         })
 
-        //Wait for window to load and then get random numbers facts from numberapi site
 
-        window.addEventListener('load', function(){
-          randomNum = Math.floor(Math.random() * 100);
+        function randomFactGenerator(){
 
-          var xhr= new XMLHttpRequest();
-        
-          xhr.onload = function(){
-           if (xhr.status === 200) {
-            var text=document.createTextNode(xhr.response);
-             factText.appendChild(text);
-           };
+          let factText=document.getElementById(`factText`);
 
-          };
+          let factorPromise = new Promise(function(resolve,reject) { 
 
-          xhr.open('GET','https://numbersapi.p.mashape.com/'+randomNum+'/trivia?mashape-key=D3M5a9cS8QmshH8z6Xqs1CiPPWVGp1kGJ61jsnJYodMZu7MFXO');
+            let xhr= new XMLHttpRequest();
+
+            let randomNum = Math.floor(Math.random() * 100);
+
+            if(factText.childNodes[0]){
+              factText.removeChild(factText.childNodes[0]);              
+            };
+
+            xhr.onload = ()=>{
+
+             if (xhr.status === 200) {
+
+               let text=document.createTextNode(xhr.response);
+
+               resolve(text);
+
+             };
+
+            };
+
+            xhr.onerror = error => reject(error);
+
+
+          xhr.open(`GET`,`https://numbersapi.p.mashape.com/${randomNum}/trivia?mashape-key=D3M5a9cS8QmshH8z6Xqs1CiPPWVGp1kGJ61jsnJYodMZu7MFXO`);
 
           xhr.send();
+
+        });
+
+          factorPromise.then(
+            function (result){
+              factText.appendChild(result);
+            },
+            function (error){
+              console.log(error);
+            }
+          );
+
+
+        }
+
+
+        //Wait for window to load and then get random numbers facts from numberapi site
+
+        window.addEventListener(`load`, () => {
+
+          randomFactGenerator();
+
         })
 
 
-        factButton.addEventListener('click',function(){
-          
-          var xhr= new XMLHttpRequest();
+        factButton.addEventListener(`click`,()=>{
 
-          randomNum = Math.floor(Math.random() * 100);
-          factText.removeChild(factText.childNodes[0]);
+          randomFactGenerator();
 
-          xhr.onload = function(){
-           if (xhr.status === 200) {
-            var text=document.createTextNode(xhr.response);
-             factText.appendChild(text);
-           };
 
-          };
-
-          xhr.open('GET','https://numbersapi.p.mashape.com/'+randomNum+'/trivia?mashape-key=D3M5a9cS8QmshH8z6Xqs1CiPPWVGp1kGJ61jsnJYodMZu7MFXO');
-
-          xhr.send();          
         })
 
 
@@ -176,43 +259,63 @@ enter.addEventListener('click',function(event){
 
             // Calculate how far and how fast to scroll
 
-            var rateOfIncrementation=25;
-            var startLocation = window.pageYOffset;
-       		var endLocation = target.offsetTop;
- 			var distance = endLocation - startLocation;
-	        var increments = distance/rateOfIncrementation;
-	        var stopAnimation;
+          let rateOfIncrementation=25;
 
-            // Scroll the page by an increment, and check if it's time to stop
-            var animateScroll = function () {
+          let startLocation = window.pageYOffset;
+
+       		let endLocation = target.offsetTop;
+
+ 			    let distance = endLocation - startLocation;
+
+	        let increments = distance/rateOfIncrementation;
+
+	        let stopAnimation;
+
+            // Scroll the page by an increment, and check if it`s time to stop
+            let animateScroll = ()=> {
+
                 window.scrollBy(0, increments);
+
                 stopAnimation();
             };
 
 
             // If scrolling down
             if ( increments >= 0 ) {
+
                 // Stop animation when you reach the anchor OR the bottom of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
+
+                stopAnimation =  () => {
+
+                    let travelled = window.pageYOffset;
+
                     if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+
                         clearInterval(runAnimation);
+
                     }
                 };
             }
+
             // If scrolling up
+
             else {
+
                 // Stop animation when you reach the anchor OR the top of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
+                stopAnimation = () => {
+
+                    let travelled = window.pageYOffset;
+
                     if ( travelled <= (endLocation || 0) ) {
+
                         clearInterval(runAnimation);
+
                     }
                 };
             }
 
             // Loop the animation function
-            var runAnimation = setInterval(animateScroll,20);
+            let runAnimation = setInterval(animateScroll,20);
        
         };
 
@@ -222,19 +325,19 @@ enter.addEventListener('click',function(event){
      //the associated inputs, which will determine which menu label will
      //get highlighted by our CSS code
 
-     [].forEach.call(menuLabel,function(element){
+     [].forEach.call(menuLabel, element => {
 
             //Determine which input is associated with the currently checked label
-           // var parentLI=element.parentElement;
-          //  var associatedInput=parentLI.previousElementSibling;
+           // let parentLI=element.parentElement;
+          //  let associatedInput=parentLI.previousElementSibling;
 
             //Determine what anchor is associated with that menuLabel
-            var associatedAnchor=element.firstElementChild;
+            let associatedAnchor=element.firstElementChild;
 
             //start scroll for the associated anchor if Label has been clicked
-            //Remember: The label holds the '<a>' anchor link
+            //Remember: The label holds the `<a>` anchor link
 
-            element.addEventListener('click', function(event){
+            element.addEventListener(`click`, event=>{
                 // Prevent the default link behavior
                 event.preventDefault(); 
 
@@ -249,39 +352,33 @@ enter.addEventListener('click',function(event){
 
             // On click of the element pointing to the anchor commence the scroll
 
-                link.addEventListener('click', letsRoll(link),false); 
+                link.addEventListener(`click`, letsRoll(link),false); 
 
 
              function letsRoll(link){ //ISUE IS THIS CLICK ON THE A LINK COMING FROM AboVE
 
-                // Prevent the default link behavior
-               // link.preventDefault();
 
                 //Check the appropriate input to highlight the label
 
-                var parentLabel=link.parentElement;
-                var parentLI=parentLabel.parentElement;
-                var associatedInput=parentLI.previousElementSibling;
-                associatedInput.checked="true";
+                let parentLabel=link.parentElement;
 
-                //create new button as soon as a scroll has started
-/**********************************************************************************
-                var allContent=document.getElementById("allContent");
-                var scrollToTop=document.createElement('BUTTON');
-                var text=document.createTextNode('Back To Top');
-                scrollToTop.appendChild(text);
-                scrollToTop.className='link2Top topNavBar__anchor';
-                allContent.appendChild(scrollToTop);
-***********************************************************************************/
+                let parentLI=parentLabel.parentElement;
+
+                let associatedInput=parentLI.previousElementSibling;
+
+                associatedInput.checked=`true`;
 
 
                 //Get the destination section where we want to end up
                 //and start the scroll
-                var targetId = link.getAttribute('href');
-                var targetElement = document.querySelector(targetId);
+
+                let targetId = link.getAttribute(`href`);
+
+                let targetElement = document.querySelector(targetId);
 
                 // If the anchor exists
                 if (targetElement) {
+
                     // Scroll to the anchor
                     scrollIt(targetElement);
                 };
@@ -292,13 +389,17 @@ enter.addEventListener('click',function(event){
 
         };
 
-                     //Check if the button clicked is the 'Back To Top' button
+                     //Check if the button clicked is the `Back To Top` button
                 //and if so, commence the scroll to the top
 
-                scrollToTop.addEventListener('click',function(event){
+                scrollToTop.addEventListener(`click`,event=>{
+
                     event.preventDefault();
-                    var topId=document.body.getAttribute('id');
-                    var top=document.querySelector('#'+topId);
+
+                    let topId=document.body.getAttribute(`id`);
+
+                    let top=document.querySelector(`#${topId}`);
+
                     scrollIt(top);
 
 
